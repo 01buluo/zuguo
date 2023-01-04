05a358645e75cd89ff58329ae599c5e3319990c1
01buluo <108160980+01buluo@users.noreply.github.com>
2023-01-04 17:30:47 +0800

    Add files via upload

--- 0.js
+++ 0.js
@@ -2685,7 +2685,7 @@ function xxqg(userinfo) {
           (toastLog("双人对战开始"), do_duizhan1(2), jifen_list = refind_jifen())
   } else true == siren && true == shuangren && sign_list.push("ocr_false");
   true == bendi && ("old" == jifen_flag && "已完成" != jifen_list.child(jifen_map["本地"]).child(3).text() || "new" == jifen_flag && "已完成" != jifen_list.child(jifen_map["本地"]).child(4).text()) && (toastLog("本地开始"), do_bendi(), jifen_list = refind_jifen());
-  if (dingyue == 2){toastLog("订阅开始"), d = do_dingyue()};
+  if (dingyue == 2){toastLog("订阅开始"), do_dingyue()};
   // d = 1;
   // 0 != dingyue && ("old" == jifen_flag && "0" == jifen_list.child(jifen_map["订阅"]).child(2).text().match(/\d+/)[0] ||
   //     "old" == jifen_flag && "0" == jifen_list.child(jifen_map["订阅"]).child(3).child(0).text()) && (toastLog("订阅开始"), d = do_dingyue(), jifen_list = refind_jifen());
