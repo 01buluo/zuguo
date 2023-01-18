244cc20a320481e9b8354ffd34def42a172b8b8c
01buluo <108160980+01buluo@users.noreply.github.com>
2023-01-18 14:38:34 +0800

    Add files via upload

--- 1.js
+++ 1.js
@@ -2242,6 +2242,8 @@ function entry_jinfen_project(a) {
   if ("old" == jifen_flag) var b = 3;
   else "new" == jifen_flag && (b = 4);
   jifen_list.child(jifen_map[a]).child(b).click()
+  if ("new" == jifen_flag) sleep(2500);
+  if(a=='挑战' && textContains('挑战答题').exists()) {fInfo("检测为新界面--分类版界面"); press(300,700,100);}
 }
 
 function winReshow() {
