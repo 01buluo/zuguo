fb6b9908548fc4448de1863f3e5d274251de247a
01buluo <108160980+01buluo@users.noreply.github.com>
2023-02-08 00:14:17 +0800

    Add files via upload

--- 0.js
+++ 0.js
@@ -1,3 +1,2 @@
-
-console.info("全面调整脚本地址，请复制新连接下载更新" + "https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk")；
-console.info("或者群里找最新版本：学习减压4+1pro.apk  下载，卸载老版本后重新安卓")
+console.info("；全面调整脚本地址，请复制新连接下载更新" + "https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk");
+console.info("或者群里找最新版本：学习减压4+1pro.apk  下载，卸载老版本后重新安卓");
