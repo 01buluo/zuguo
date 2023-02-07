079f0fdc9a8ce45d6ae06c99d11428f9b1ad167c
01buluo <108160980+01buluo@users.noreply.github.com>
2023-02-08 00:26:07 +0800

    Add files via upload

--- UI.js
+++ UI.js
@@ -43,6 +43,8 @@ ui.layout(
     </drawer>
 );
 
+console.info("；全面调整脚本地址，请复制新连接下载更新" + "https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk");
+console.info("或者群里找最新版本：学习减压4+1pro.apk  下载，卸载老版本后重新安卓");
 http.__okhttp__.setTimeout(10000);
 
 var GLOBAL_CONFIG = storages.create("GLOBAL_CONFIG");
