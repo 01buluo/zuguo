44684eb0c98a45679d5621392ff52b943685621c
01buluo <108160980+01buluo@users.noreply.github.com>
2023-02-08 00:43:46 +0800

    Add files via upload

--- UI.js
+++ UI.js
@@ -10,7 +10,7 @@ console.clear();
 ui.主题颜色 = "#FFC0CB";
 ui.标题 = "学习四合一测试版pro(从疫情中走出，专心于工作)";
 ui.副标题 = "让各位从疫情走出来专心于工作";
-ui.公告 = "1.仅供个人测试使用（四合一）pro全新上线\n2.群里找最新版本：学习减压4+1pro.apk下载，卸载老版本后重新安卓！\n3.试用期过后，请赞助获取卡密（Q群3：758116397，加群获得最新apk和资料）。\n4.root去除截图权限版适合最新版，适用于手机root或虚拟机或模拟器通过模块去除截图限制等。\n5.全面调整脚本地址，请复制新连接下载更新" + "https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk";
+ui.公告 = "1.仅供个人测试使用（四合一）pro全新上线\n2.群里找最新版本：学习减压4+1pro.apk下载，卸载老版本后重新安卓！\n3.试用期过后，请赞助获取卡密（Q群3：758116397，加群获得最新apk和资料）。\n4.root去除截图权限版适合最新版，适用于手机root或虚拟机或模拟器通过模块去除截图限制等。\n5.全面调整脚本地址，点击右上角三个点---下拉菜单---最新版下载---输入密码：ex28\n或请复制新连接下载更新" + "https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk";
 
 var vip = 1;//VIP权限自由开关
 var color = "#FF4FB3FF";
@@ -79,10 +79,10 @@ ui.emitter.on("options_item_selected", (e, item) => {
             app.startActivity("console");
             break;
         case "关于":
-            alert("关于", "学习测试四合一PRO v" + latest_version + "\n  新Q群：758116397");
+            alert("关于", "学习减压四合一PRO v" + latest_version + "\n  新Q群：758116397");
             break;
-        case "Github":
-            app.openUrl("https://gitee.com/djh010/xuexiqiangguo---xxqg/blob/master/%E5%AD%A6%E4%B9%A0%E5%87%8F%E5%8E%8B4+1pro_2.3.0_sign.apk");
+        case "最新版下载（密码：ex28）":
+            app.openUrl("https://wwsc.lanzouo.com/imSwd0mvp0ze");
             break;
         case "V2.33.0下载":
             app.openUrl("https://android-apps.pp.cn/fs08/2021/12/28/3/110_f37c420b0944cb7b9f60a2ad9b5518d2.apk?yingid=web_space&packageid=500730793&md5=664bb7bdcae57be189fc86100f4371c4&minSDK=21&size=191654161&shortMd5=1fee0bd160d08108a9d9e5f4773ce741&crc32=3879122865&did=ad484a175e19d0928044435e24bf03cb");
